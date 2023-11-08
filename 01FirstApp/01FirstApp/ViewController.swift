1404b3fc4aeba47b56bcfc0eac41abb0d9b460e1
bbell428 <428bbell@gmail.com>
2023-11-08 10:01:49 +0900

    FristApp

---
+++ 01FirstApp/01FirstApp/ViewController.swift
@@ -0,0 +1,20 @@
+//  ViewController.swift
+
+//스토리보드 UI에 대응하기 위한 컨트롤소스
+//한 화면 - 스토리보드 화면 1개 - 뷰컨트롤러 소스 1개
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        //뷰가 로딩되었을 때 자동 호출되는 함수
+        //초기화 코드들
+    }
+
+
+}
+
