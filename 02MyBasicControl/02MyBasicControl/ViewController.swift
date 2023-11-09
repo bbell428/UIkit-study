5be256fb057abd1226c1a4bfd23ef24cec2d46d6
bbell428 <428bbell@gmail.com>
2023-11-09 23:15:54 +0900

    MyBasicContol

--- 02MyBasicControl/02MyBasicControl/ViewController.swift
+++ 02MyBasicControl/02MyBasicControl/ViewController.swift
@@ -15,7 +15,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var btn1: UIButton!
     @IBOutlet weak var btn2: UIButton!
     @IBOutlet weak var btn3: UIButton!
-    @IBOutlet var buttons: [UIButton]!
+    @IBOutlet var buttons: [UIButton]! // 배열 가능
+    @IBOutlet weak var tfInput: UITextField!
+    @IBOutlet weak var imageView: UIImageView!
     
     
     
@@ -28,6 +30,14 @@ class ViewController: UIViewController {
         labelTitle.textColor = UIColor.orange
         
         btn1.backgroundColor = UIColor.red
+        
+        for btn in buttons { // 버튼 순환
+            btn.backgroundColor = UIColor(red: 1, green: 0, blue: 0, alpha: 1)
+            //alpha: 0.0 ~ 1.0 (투명도)
+            //GRB: 0.0~1.0
+            btn.backgroundColor = UIColor(red: 255/255, green: 100/255, blue: 100/255, alpha: 1)
+            buttons[2].backgroundColor = UIColor.blue
+        }
     }
 
     //액션 함수가 들어가는 곳
@@ -35,9 +45,20 @@ class ViewController: UIViewController {
         //로그를 남기는 법
         print(" btn1 클릭 ")
         labelTitle.text = "btn1 클릭됨"
+        imageView.image = UIImage(named: "image1.png")
+    }
+    @IBAction func onebtn2Click(_ sender: UIButton) {
+        imageView.image = UIImage(named: "image2.png")
+    }
+
+    @IBAction func onbtnInput(_ sender: UIButton) {
+        print("onbtnInput 클릭됨")
+        let titleText = tfInput.text
+        labelTitle.text = titleText
     }
     
     
+    //배경화면 단축키
+    //CMD + SHIFT + 4
     
 }
-
