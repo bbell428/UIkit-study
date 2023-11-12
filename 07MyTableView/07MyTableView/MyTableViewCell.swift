2256f38a9f08230602f87e12c95c0bea1d7d3954
bbell428 <428bbell@gmail.com>
2023-11-13 02:07:36 +0900

    1

---
+++ 07MyTableView/07MyTableView/MyTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  Created by 김종혁 on 11/13/23.
+
+
+import UIKit
+
+class MyTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var idolImageView: UIImageView!
+    @IBOutlet weak var labelName: UILabel!
+    @IBOutlet weak var labelArt: UILabel!
+    
+    
+}
