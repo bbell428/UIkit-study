2fec88e71c9c5fa6836ae5ecb6e4230b7137c357
bbell428 <428bbell@gmail.com>
2023-11-23 12:24:01 +0900

    study

--- 01FirstApp/01FirstApp/ViewController.swift
+++ 01FirstApp/01FirstApp/ViewController.swift
@@ -5,11 +5,14 @@
 
 
 import UIKit
+// UIKit 프레임워크를 가져와서 iOS 앱을 개발할 때 필요한 기본 라이브러리와 클래스를 사용할 수 있도록 합니다.
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
+        // 함수를 재정의(override)
         super.viewDidLoad()
+        //super.viewDidLoad()은 현재 클래스가 상속받은 슈퍼클래스(부모 클래스)인 UIViewController의 viewDidLoad() 메서드를 호출하는 것입니다.
         
         //뷰가 로딩되었을 때 자동 호출되는 함수
         //초기화 코드들
